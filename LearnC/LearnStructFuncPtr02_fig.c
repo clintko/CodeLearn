5d509d7a1f774260b6a3ebe2a5d4c4cd6b0d7968
unknown <clint00724@gmail.com>
2015-10-09 22:55:55 +0800

    update struct Fig; construct 2d array to store frame and add moving method, check method

--- LearnC/LearnStructFuncPtr02_fig.c
+++ LearnC/LearnStructFuncPtr02_fig.c
@@ -1,14 +1,75 @@
 /**/
 #include <stdio.h>
 #include <stdlib.h>
+//#include "LearnStructFuncPtr01_point.h"
 #include "LearnStructFuncPtr02_fig.h"
 
 int main(){
 
-    fig_gun01 = pic_2dto1d(4, 20, pic_gun01);
-    pic_print(fig_gun01, 4, 20);
+    Fig* gun01 = Fig_init(10, 20, 4, 20, 2);
+    gun01->pic[0] = fig_2dto1d(4, 20, pic_gun01);
+    gun01->pic[1] = fig_2dto1d(4, 20, pic_gun02);
+    
+    printf("\nAfter Initialization\n");
+    gun01->print(gun01);
+    gun01->frame_cur += 1;
+    gun01->print(gun01);
+    
+    
+    printf("\nCheck getter function and set function of struct Fig\n");
+    printf("The current size of figure\nHeight: %d\nWidth: %d\n",
+	   gun01->getHeight(gun01), gun01->getWidth(gun01));
 
-    free(fig_gun01);
+    printf("The current position of figure\nx: %d\ny: %d\n",
+	   gun01->getX(gun01), gun01->getY(gun01));
+
+    printf("\nCurrent Location\n");
+    gun01->pos->print(gun01->pos);
+
+    printf("After Move Down\n");
+    gun01->moveDown(gun01);
+    gun01->pos->print(gun01->pos);
+
+    printf("Test Check\n");
+    printf("Initialize a point\n");
+    
+    Point* pixel = Point_init(10, 20);
+    pixel->print(pixel);
+
+    if (gun01->check(gun01, pixel)){
+	printf("Point in Figure\n");
+    } else {
+	printf("Point not in Figure\n");
+    } // end if-else
+    printf("\n");
+    
+    printf("Move Down the point\n");
+    pixel->moveDown(pixel);
+    pixel->print(pixel);
+
+    if (gun01->check(gun01, pixel)){
+	printf("Point in Figure\n");
+    } else {
+	printf("Point not in Figure\n");
+    } // end if-else
+    printf("\n");
+
+    printf("Move Down the point\n");
+    pixel->moveDown(pixel);
+    pixel->print(pixel);
+
+    if (gun01->check(gun01, pixel)){
+	printf("Point in Figure\n");
+    } else {
+	printf("Point not in Figure\n");
+    } // end if-else
+    printf("\n");
+
+    
+    free(gun01->pos);
+    free(gun01->pic);
+    free(gun01);
+    free(pixel);
     
 
     
