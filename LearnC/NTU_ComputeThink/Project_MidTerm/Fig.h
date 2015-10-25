cb4c003bad6188a1109ae96e3a0df25a582df76d
unknown <clint00724@gmail.com>
2015-10-25 22:55:03 +0800

    Fig.h and test.c updated; getter, setter, addPic

--- LearnC/NTU_ComputeThink/Project_MidTerm/Fig.h
+++ LearnC/NTU_ComputeThink/Project_MidTerm/Fig.h
@@ -30,61 +30,93 @@ typedef struct Fig{
                    // the number of rows is the total number of frame(pictures)
 
     // method
-    void (*print)(struct Fig*);     // print out the current frame
-    int (*getX)(struct Fig*);       // Getter Method; get x coordinate
-    int (*getY)(struct Fig*);       // Getter Method; get y coordinate
-    int (*getHeight)(struct Fig*);  // Getter Method; get figure height
-    int (*getWidth)(struct Fig*);   // Getter Method; get figure width
-    int (*moveUp)(struct Fig*);     // shift the picture one step up
-    int (*moveDown)(struct Fig*);   // shift the picture one step down
-    int (*moveLeft)(struct Fig*);   // shift the picture one step left
-    int (*moveRight)(struct Fig*);  // shift the picture one step right
+    void (*print)(struct Fig*);      // print out the current frame
+    int (*getX)(struct Fig*);        // Getter Method; get x coordinate
+    int (*getY)(struct Fig*);        // Getter Method; get y coordinate
+    int (*getHeight)(struct Fig*);   // Getter Method; get figure height
+    int (*getWidth)(struct Fig*);    // Getter Method; get figure width
+    int (*getFrameTot)(struct Fig*); // Getter Method;
+                                     // get the total number of frame
+    int (*getFrameCur)(struct Fig*); // Getter Method;
+                                     // get the current index of frame
+    void (*setX)(struct Fig*, int);  // Setter Method; set x coordinate
+    void (*setY)(struct Fig*, int);  // Setter Method; set y coordinate
+    void (*reset)(struct Fig*);      // reset the figure to the origin
+    int (*moveUp)(struct Fig*);      // shift the picture one step up
+    int (*moveDown)(struct Fig*);    // shift the picture one step down
+    int (*moveLeft)(struct Fig*);    // shift the picture one step left
+    int (*moveRight)(struct Fig*);   // shift the picture one step right
+    //void (*addPic)(struct Fig*, char*); // add picture to the figure
+    void (*addPic)(struct Fig*self, int height, int width, char pic[height][width+1]);
     int (*check)(struct Fig*, struct Point*);
-
+    void (*freeFig)(struct Fig*);    // free the memory of Fig
+    
 }Fig;
 
 // Function Prototype
-Fig* Fig_init(int, int, int, int, int);
+Fig* Fig_init(int, int, int, int);
 void Fig_print(Fig*);
 int Fig_getX(Fig*);
 int Fig_getY(Fig*);
 int Fig_getHeight(Fig*);
 int Fig_getWidth(Fig*);
+int Fig_getFrameTot(Fig*);
+int Fig_getFrameCur(Fig*);
+void Fig_setX(Fig*, int);
+void Fig_setY(Fig*, int);
+void Fig_reset(Fig*);
 int Fig_moveUp(Fig*);
 int Fig_moveDown(Fig*);
 int Fig_moveLeft(Fig*);
 int Fig_moveRight(Fig*);
+//void Fig_addPic(Fig*, char*);
+void Fig_addPic(Fig* self, int height, int width, char pic[height][width+1]);
 int Fig_check(Fig*, Point*);
 int Fig_isPointInFig(Fig*, Point*);
+void Fig_free(Fig*);
 
 char* fig_2dto1d(int height, int width, char pic_d2[height][width+1]);
 
 
 // Implement Functions
-Fig* Fig_init(int x, int y, int height, int width, int frame_tot){
-    Fig* temp = malloc(sizeof(struct Fig));
+Fig* Fig_init(int x, int y, int height, int width){
+    Fig* self = malloc(sizeof(struct Fig));
     // initialized attributes
-    temp->pos = Point_init(x, y);
-    temp->height = height;
-    temp->width = width;
-    temp->frame_tot = frame_tot;
-    temp->frame_cur = 0;
+    self->pos = Point_init(x, y);
+    self->height = height;
+    self->width = width;
+    self->frame_tot = 0;
+    self->frame_cur = 0;
 
     // dynamic allocate pictures
-    temp->pic = malloc(frame_tot * sizeof(char*));
-
-    // assigned methods
-    temp->print = Fig_print;
-    temp->getX = Fig_getX;
-    temp->getY = Fig_getY;
-    temp->getHeight = Fig_getHeight;
-    temp->getWidth = Fig_getWidth;
-    temp->moveUp = Fig_moveUp;
-    temp->moveDown = Fig_moveDown;
-    temp->moveLeft = Fig_moveLeft;
-    temp->check = Fig_check;
+    self->pic = malloc(self->frame_tot * sizeof(char*));
     
-    return temp;
+    // initialize the picture array
+    int counter = 0;
+    for (counter = 0; counter < (self->frame_tot); counter += 1){
+	self->pic[counter] = 0;
+    } // end for loop
+    
+    // assigned methods
+    self->print = Fig_print;
+    self->getX = Fig_getX;
+    self->getY = Fig_getY;
+    self->getHeight = Fig_getHeight;
+    self->getWidth = Fig_getWidth;
+    self->getFrameTot = Fig_getFrameTot;
+    self->getFrameCur = Fig_getFrameCur;
+    self->setX = Fig_setX;
+    self->setY = Fig_setY;
+    self->reset = Fig_reset;
+    self->moveUp = Fig_moveUp;
+    self->moveDown = Fig_moveDown;
+    self->moveLeft = Fig_moveLeft;
+    self->moveRight = Fig_moveRight;
+    self->addPic = Fig_addPic;
+    self->check = Fig_check;
+    self->freeFig = Fig_free;
+    
+    return self;
 }
 
 void Fig_print(Fig* temp){
@@ -101,39 +133,120 @@ void Fig_print(Fig* temp){
 }
 
 int Fig_getX(Fig* temp){
+    /* Getter method; get the x coordinate */
     return temp->pos->getX(temp->pos);
 }
 
-int Fig_getY(Fig* temp){
-    return temp->pos->getY(temp->pos);
+int Fig_getY(Fig* self){
+    /* Getter method; get the y coordinate */
+    return self->pos->getY(self->pos);
 }
 
-int Fig_getHeight(Fig *temp){
-    return temp->height;
+int Fig_getHeight(Fig *self){
+    /* Getter method; get the height of figure */
+    return self->height;
 }
 
-int Fig_getWidth(Fig *temp){
-    return temp->width;
+int Fig_getWidth(Fig *self){
+    /* Getter method; get the width of figure*/
+    return self->width;
 }
 
-int Fig_moveUp(Fig *temp){
-    temp->pos->moveUp(temp->pos);
+int Fig_getFrameTot(Fig* self){
+    /* Getter method; get the total number of 
+       pictures in the figure */
+    return self->frame_tot;
 }
 
-int Fig_moveDown(Fig* temp){
-    temp->pos->moveDown(temp->pos);
+int Fig_getFrameCur(Fig* self){
+    /* Getter method; get the current index of 
+       pictures in the figure */
+    return self->frame_cur;
 }
 
-int Fig_moveLeft(Fig* temp){
-    temp->pos->moveLeft(temp->pos);
+void Fig_setX(Fig *self, int x){
+    /* Setter method; set the x coordinate */
+    self->pos->setX(self->pos, x);
 }
 
-int Fig_moveRight(Fig* temp){
-    temp->pos->moveRight(temp->pos);
+void Fig_setY(Fig *self, int y){
+    /* Setter method; set the y coordinate */
+    self->pos->setY(self->pos, y);
+}
+
+void Fig_reset(Fig *self){
+    /* reset the figure to the origin */
+    self->setX(self, 0);
+    self->setY(self, 0);
+}
+
+int Fig_moveUp(Fig *self){
+    /* move the figure one step up by decrementing the y coordinate */
+    self->pos->moveUp(self->pos);
+}
+
+int Fig_moveDown(Fig* self){
+    /* move the figure one step dwon by incrementing the y coordinate */
+    self->pos->moveDown(self->pos);
+}
+
+int Fig_moveLeft(Fig* self){
+    /* move the figure one step left by decrementing the x coordinate */
+    self->pos->moveLeft(self->pos);
+}
+
+int Fig_moveRight(Fig* self){
+    /* move the figure one step right by incrementing the x coordinate */
+    self->pos->moveRight(self->pos);
+}
+
+//void Fig_addPic(Fig* self, char pic[self->height][self->width+1]){
+void Fig_addPic(Fig* self, int height, int width, char pic[height][width+1]){
+    /* add a picture into the figure */
+
+    // dynamic allocate picture array
+    char **temp_pic = malloc((self->frame_tot + 1) * sizeof(char*));
+    int counter;
+    
+    // initialize the picture array
+    for (counter = 0; counter < (self->frame_tot + 1); counter += 1){
+	temp_pic[counter] = 0;
+    } // end for loop
+    
+    // copy the content from self->pic to temp_pic
+    for (counter = 0; counter < (self->frame_tot); counter += 1){
+        temp_pic[counter] = self->pic[counter];	
+    } // end for loop
+
+    // assign the new picture
+    //temp_pic[self->frame_tot] = pic;
+    temp_pic[self->frame_tot] = fig_2dto1d(self->height, self->width, pic);
+	
+    // free the original picture array of figures
+    free(self->pic);
+
+    // assigned the new pictures to figures
+    self->pic = temp_pic;
+    
+    // increment the total frame
+    self->frame_tot += 1;
 }
 
 int Fig_check(Fig* fig, Point* point){
     return isPointInFig(fig, point);
+}
+
+void Fig_free(Fig* temp){
+    int counter;
+    // free the picture in the Figure
+    for(counter = 0; counter < temp->frame_tot; counter += 1){
+	// free each picture
+	free(temp->pic[counter]);
+    } // end for loop
+    free(temp->pic);
+
+    // free the Figure
+    free(temp);
 }
 
 int isPointInFig(Fig* fig, Point* point){
@@ -162,8 +275,13 @@ int isPointInFig(Fig* fig, Point* point){
 // helper function
 char* fig_2dto1d(int height, int width, char pic_d2[height][width+1]){
     /* convert 2D array to 1D */
-    char* pic_d1 = malloc(height * width * sizeof(char));
+    char* pic_d1 = malloc(height * width * sizeof(char) + 1);
     int idx1, idx2;
+
+    // initialize the array
+    for(idx1 = 0; idx1 < (height * width + 1); idx1 += 1){
+    	pic_d1[idx1] = 0;
+    } // end for loop
     
     // specified each element to 1D array from input 2D array
     for (idx1 = 0; idx1 < height; idx1 += 1){
@@ -173,7 +291,7 @@ char* fig_2dto1d(int height, int width, char pic_d2[height][width+1]){
 	    
 	} // end inner loop
     } // end outer loop
-
+    
     return pic_d1;
 }
 
--- LearnC/NTU_ComputeThink/Project_MidTerm/test.c
+++ LearnC/NTU_ComputeThink/Project_MidTerm/test.c
@@ -101,22 +101,229 @@ void testStructPoint(){
 
 char test_pic11[1][1] = {"A"};
 char test_pic12[1][1] = {"B"};
-char test_pic21[2][2] = {"AB", "CD"};
-char test_pic22[2][2] = {"XY", "ZW"};
+char test_pic21[2][3] = {"AB", "CD"};
+char test_pic22[2][3] = {"XY", "ZW"};
 
 void testStructFig(){
     printf("Test the structure Fig\n");
     printf("==========================================\n");
     printf("Initialize a Fig\n");
+    Fig* fig01 = Fig_init(10, 20, 1, 1);
+    Fig* fig02 = Fig_init( 5, 10, 2, 2);
+    
     printf("Check Getter Method\n");
+    
+    if(fig01->getX(fig01) == 10){
+	printf("    test11: getX; Passed\n");
+    }else{
+	printf("    test11: getX; Failed\n");
+    } // end if-else
+
+    if(fig01->getY(fig01) == 20){
+	printf("    test12: getY; Passed\n");
+    }else{
+	printf("    test12: getY; Failed\n");
+    } // end if-else
+
+    // check getHeight and getWidth
+    if (fig01->getHeight(fig01) == 1){
+	printf("    test13: getHeight; Passed\n");
+    } else {
+	printf("    test13: getHeight; Failed\n");
+    }
+
+    if (fig02->getHeight(fig02) == 2){
+	printf("    test14: getHeight; Passed\n");
+    } else {
+	printf("    test14: getHeight; Failed\n");
+    }
+
+    if (fig01->getWidth(fig01) == 1){
+	printf("    test15: getWidth; Passed\n");
+    } else {
+	printf("    test15: getWidth; Failed\n");
+    }
+
+    if (fig02->getWidth(fig02) == 2){
+	printf("    test16: getWidth; Passed\n");
+    } else {
+	printf("    test16: getWidth; Failed\n");
+    }
+
+    // check getFrameTot and getFrameCur
+    if (fig01->getFrameTot(fig01) == 0){
+	printf("    test17: getFrameTot; Passed\n");
+    } else {
+	printf("    test17: getFrameTot; Failed\n");
+    }
+
+    if (fig01->getFrameCur(fig01) == 0){
+	printf("    test18: getFrameCur; Passed\n");
+    } else {
+	printf("    test18: getFrameCur; Failed\n");
+    }
+    
+    // ------------------------------------------    
     printf("Check Setter Method\n");
+    
+    fig01->setX(fig01, 35);
+    if(fig01->getX(fig01) == 35){
+	printf("    test21: setX; Passed\n");
+    }else{
+	printf("    test21: setX; Failed\n");
+    } // end if-else
+    
+    fig01->setY(fig01, 55);
+    if(fig01->getY(fig01) == 55){
+	printf("    test22: getY; Passed\n");
+    }else{
+	printf("    test22: getY; Failed\n");
+    } // end if-else
+
+    // -----------------------------------------
+    printf("Check Method reset\n");
+    fig01->reset(fig01);
+    
+    if((fig01->getX(fig01) == 0) && (fig01->getY(fig01) == 0)){
+	printf("    test31: reset; Passed\n");
+    }else{
+	printf("    test31: reset; Failed\n");
+    } // end if-else
+    
+    // ------------------------------------------
     printf("Check Method move\n");
+    
+    fig01->moveUp(fig01);
+    if(fig01->getY(fig01) == -1){
+	printf("    test41: moveUp; Passed\n");
+    }else{
+	printf("    test41: moveUp; Failed\n");
+    } // end if-else
+    
+    fig01->moveDown(fig01);
+    fig01->moveDown(fig01);
+    if(fig01->getY(fig01) == 1){
+	printf("    test42: moveDown; Passed\n");
+    }else{
+	printf("    test42: moveDown; Failed\n");
+    } // end if-else
+    
+    fig01->moveLeft(fig01);
+    if(fig01->getX(fig01) == -1){
+	printf("    test43: moveLeft; Passed\n");
+    }else{
+	printf("    test43: moveLeft; Failed\n");
+    } // end if-else
+    
+    fig01->moveRight(fig01);
+    fig01->moveRight(fig01);
+    if(fig01->getX(fig01) == 1){
+	printf("    test44: moveRight; Passed\n");
+    }else{
+	printf("    test44: moveRight; Failed\n");
+    } // end if-else
+
+    // -----------------------------------------
+    printf("Check Helper Function fig_2dto1d\n");
+    
+    // printf("%s\n", fig_2dto1d(1, 1, test_pic11));
+    if((strcmpi(fig_2dto1d(1, 1, test_pic11), "A") == 0)){
+	printf("    test51: fig_2dto1d; Passed\n");
+    } else {
+	printf("    test51: fig_2dto1d; Failed\n");
+    } // end if-else
+
+    //printf("%s\n", fig_2dto1d(1, 1, test_pic12));
+    if((strcmpi(fig_2dto1d(1, 1, test_pic12), "B") == 0)){
+	printf("    test52: fig_2dto1d; Passed\n");
+    } else {
+	printf("    test52: fig_2dto1d; Failed\n");
+    } // end if-else
+
+    //printf("%s\n", fig_2dto1d(2, 2, test_pic21));
+    if((strcmpi(fig_2dto1d(2, 2, test_pic21), "ABCD") == 0)){
+	printf("    test53: fig_2dto1d; Passed\n");
+    } else {
+	printf("    test53: fig_2dto1d; Failed\n");
+    } // end if-else
+
+    //printf("%s\n", fig_2dto1d(2, 2, test_pic22));
+    if((strcmpi(fig_2dto1d(2, 2, test_pic22), "XYZW") == 0)){
+	printf("    test54: fig_2dto1d; Passed\n");
+    } else {
+	printf("    test54: fig_2dto1d; Failed\n");
+    } // end if-else
+    // ------------------------------------------
     printf("Check Method addPic\n");
+
+    // fig01 add test picture 11
+    //original version: fig01->addPic(fig01, fig_2dto1d(1, 1, test_pic11));
+    fig01->addPic(fig01,
+		  fig01->getWidth(fig01),
+		  fig01->getHeight(fig01),
+		  test_pic11);
+    if((fig01->getFrameTot(fig01) == 1) &&
+       (strcmpi(fig01->pic[0], "A") == 0)){
+	printf("    test61: addPic; Passed\n");
+    } else {
+	printf("    test61: addPic; Failed\n");
+    } // end if-else
+
+    // fig01 add test picture 12
+    // original version: fig01->addPic(fig01, fig_2dto1d(1, 1, test_pic12));
+    fig01->addPic(fig01,
+		  fig01->getWidth(fig01),
+		  fig01->getHeight(fig01),
+		  test_pic12);
+    if((fig01->getFrameTot(fig01) == 2) &&
+       (strcmpi(fig01->pic[1], "B") == 0)){
+	printf("    test62: addPic; Passed\n");
+    } else {
+	printf("    test62: addPic; Failed\n");
+    } // end if-else
+       
+    // fig02 add test picture 21
+    fig02->addPic(fig02,
+		  fig02->getWidth(fig02),
+		  fig02->getHeight(fig02),
+		  test_pic21);
+    if((fig02->getFrameTot(fig02) == 1) &&
+       (strcmpi(fig02->pic[0], "ABCD") == 0)){
+	printf("    test63: addPic; Passed\n");
+    } else {
+	printf("    test63: addPic; Failed\n");
+    } // end if-else
+
+    // fig02 add test picture 22
+    fig02->addPic(fig02,
+		  fig02->getWidth(fig02),
+		  fig02->getHeight(fig02),
+		  test_pic22);
+    if((fig02->getFrameTot(fig02) == 2) &&
+       (strcmpi(fig02->pic[1], "XYZW") == 0)){
+	printf("    test64: addPic; Passed\n");
+    } else {
+	printf("    test64: addPic; Failed\n");
+    } // end if-else
+    
+    // ------------------------------------------
+    printf("Check Method get\n");
+    
+    // ------------------------------------------
     printf("Check Method Print\n");
+
+    // ------------------------------------------
     printf("Check Method nextPic\n");
+
+    // ------------------------------------------
     printf("Check Method isPointInFig\n");
+
+    // ------------------------------------------
     printf("Check Method check\n");
+    
+    // ----------------------------------------------
     printf("Free the memory of Fig\n");
+    fig01->freeFig(fig01);
     printf("Finish testing structure Fig\n");
     printf("==========================================\n");
 }
